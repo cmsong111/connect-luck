5eba3656cf2bebecece567bcf6a1b798b14e32ca
Namju Kim <cmsong111@naver.com>
2024-03-14 02:27:14 +0900

    style: formatting code style
    with intelliJ

--- src/main/java/ac/kr/deu/connect/luck/user/UserRepository.java
+++ src/main/java/ac/kr/deu/connect/luck/user/UserRepository.java
@@ -8,7 +8,9 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
     Optional<User> findByEmail(String email);
+
     Optional<User> findByEmailAndPassword(String email, String password);
 
 }
