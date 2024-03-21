45bea27d0c0b2fabcd20aecb1b4955049307ba82
Namju Kim <cmsong111@naver.com>
2024-03-21 17:46:39 +0900

    Docs: FoodTruck Service 주석 추가

--- src/main/java/ac/kr/deu/connect/luck/exception/CustomErrorCode.java
+++ src/main/java/ac/kr/deu/connect/luck/exception/CustomErrorCode.java
@@ -11,7 +11,8 @@ public enum CustomErrorCode {
     EMAIL_NOT_FOUND(HttpStatus.BAD_REQUEST, "이메일을 찾을 수 없습니다."),
     PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     PHONE_NOT_FOUND(HttpStatus.BAD_REQUEST, "휴대폰 번호를 찾을 수 없습니다."),
-    ID_NOT_MATCH(HttpStatus.BAD_REQUEST, "유저 번호가 일치하지 않습니다.");
+    ID_NOT_MATCH(HttpStatus.BAD_REQUEST, "유저 번호가 일치하지 않습니다."),
+    FOOD_TRUCK_NOT_FOUND(HttpStatus.BAD_REQUEST, "푸드트럭을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/ac/kr/deu/connect/luck/food_truck/FoodTruckService.java
+++ src/main/java/ac/kr/deu/connect/luck/food_truck/FoodTruckService.java
@@ -1,6 +1,11 @@
 package ac.kr.deu.connect.luck.food_truck;
 
 import ac.kr.deu.connect.luck.configuration.MapStructMapper;
+import ac.kr.deu.connect.luck.exception.CustomErrorCode;
+import ac.kr.deu.connect.luck.exception.CustomException;
+import ac.kr.deu.connect.luck.user.User;
+import ac.kr.deu.connect.luck.user.UserRepository;
+import ac.kr.deu.connect.luck.user.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,25 +17,61 @@ public class FoodTruckService {
     private final FoodTruckRepository foodTruckRepository;
     private final FoodTruckMenuRepository foodTruckMenuRepository;
     private final FoodTruckReviewRepository foodTruckReviewRepository;
+    private final UserRepository userRepository;
     private final MapStructMapper mapStructMapper;
 
+    /**
+     * 모든 푸드트럭을 조회합니다.
+     *
+     * @return 모든 푸드트럭 정보
+     */
     public List<FoodTruck> getFoodTrucks() {
         return foodTruckRepository.findAll();
     }
 
+    /**
+     * ID를 통해 특정 푸드트럭을 조회합니다.
+     *
+     * @param id 조회할 푸드트럭의 ID
+     * @return 특정 푸드트럭 정보
+     */
     public FoodTruck getFoodTruck(Long id) {
         return foodTruckRepository.findById(id).orElseThrow();
     }
 
+    /**
+     * 푸드트럭을 추가합니다.
+     *
+     * @param foodTruckRequest 추가할 푸드트럭 정보
+     * @return 저장된 푸드트럭 정보
+     */
     public FoodTruck saveFoodTruck(FoodTruckRequest foodTruckRequest) {
         FoodTruck foodTruck = mapStructMapper.toFoodTruck(foodTruckRequest);
         return foodTruckRepository.save(foodTruck);
     }
 
+    /**
+     * 푸드트럭을 삭제합니다.
+     * 푸드트럭과 관련된 메뉴, 리뷰를 모두 삭제합니다.
+     *
+     * @param id 삭제할 푸드트럭의 ID
+     */
     public void deleteFoodTruck(Long id) {
-        foodTruckRepository.deleteById(id);
+        FoodTruck foodTruck = foodTruckRepository.findById(id).orElseThrow(
+                () -> new CustomException(CustomErrorCode.FOOD_TRUCK_NOT_FOUND)
+        );
+        foodTruckReviewRepository.deleteAllByFoodTruck(foodTruck);
+        foodTruckMenuRepository.deleteAllByFoodTruck(foodTruck);
+        foodTruckRepository.delete(foodTruck);
     }
 
+    /**
+     * 푸드트럭 정보를 수정합니다. 수정할 정보가 없는 경우 기존 정보를 유지합니다.
+     *
+     * @param id               푸드트럭 ID
+     * @param foodTruckRequest 수정할 푸드트럭 정보
+     * @return 수정된 푸드트럭 정보
+     */
     public FoodTruck updateFoodTruck(Long id, FoodTruckRequest foodTruckRequest) {
         FoodTruck foodTruck = foodTruckRepository.findById(id).orElseThrow();
         if (foodTruckRequest.name() != null) {
@@ -45,7 +86,18 @@ public class FoodTruckService {
         if (foodTruckRequest.foodType() != null) {
             foodTruck.setFoodType(foodTruckRequest.foodType());
         }
-
         return foodTruckRepository.save(foodTruck);
+    }
+
+    /**
+     * 푸드트럭 매니저가 되려는 사용자의 역할을 변경합니다.
+     *
+     * @param userId 푸드트럭 매니저가 되려는 사용자의 ID
+     * @return 저장된 사용자 정보
+     */
+    public User becomeFoodTruckManager(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        user.setRole(UserRole.FOOD_TRUCK_MANAGER);
+        return userRepository.save(user);
     }
 }
