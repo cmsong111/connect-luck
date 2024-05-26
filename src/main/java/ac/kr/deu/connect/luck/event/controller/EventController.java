7c360fc57309066dc31a6a6c09bc5fc616c8d845
KMINGON <rlaalsrhs59@gmail.com>
2024-05-27 03:41:54 +0900

    이벤트 CRUD 페이지 제작 및 기능 구현 및 서비스 로직 일부 수정

--- src/main/java/ac/kr/deu/connect/luck/event/controller/EventController.java
+++ src/main/java/ac/kr/deu/connect/luck/event/controller/EventController.java
@@ -2,12 +2,13 @@ package ac.kr.deu.connect.luck.event.controller;
 
 import ac.kr.deu.connect.luck.event.EventService;
 import ac.kr.deu.connect.luck.event.EventStatus;
+import ac.kr.deu.connect.luck.event.dto.EventRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.security.Principal;
 
 @Controller
@@ -19,7 +20,7 @@ public class EventController {
 
     @GetMapping
     public String getEvent(Model model) {
-        model.addAttribute("events", eventService.getEvents((EventStatus) null));
+        model.addAttribute("events", eventService.getEvents((EventStatus) null)); //나중에 인자 신청 가능한 이벤트로 바꿔야함
         return "event/event-list";
     }
 
@@ -29,11 +30,55 @@ public class EventController {
         return "event/my_event_list";
     }
 
-    //자세히 보기 페이지 매핑 메서드이나 자세히 보기를 사용하지 않을 예정, event-form 구현 안함
     @GetMapping("/{id}")
     public String getEventDetail(@PathVariable("id") Long id, Model model) {
         model.addAttribute("event", eventService.getEvent(id));
         return "event/event-form";
     }
 
+    @GetMapping("/register")
+    public String getEventRegister(Model model) {
+        return "event/event_register";
+    }
+
+    @PostMapping("/create")
+    public String registerEventPost(
+            Principal principal,
+            EventRequest eventRequest, @RequestParam("multipartFile") MultipartFile multipartFile
+    ){
+        eventService.createEvent(eventRequest, multipartFile, principal.getName());
+        return "redirect:/event/my";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String getEventDelete(@PathVariable("id") Long id, Principal principal) {
+        eventService.deleteEvent(id, principal.getName());
+        return "redirect:/event/my";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String getEventUpdate(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("event", eventService.getEvent(id));
+        return "event/event_update";
+    }
+
+    @PostMapping("/update")
+    public String updateEventPost(
+            @RequestParam("eventId") Long id,
+            Principal principal,
+            EventRequest eventRequest,
+            @RequestParam("multipartFile") MultipartFile multipartFile
+    ){
+        eventService.updateEvent(id, eventRequest, multipartFile, principal.getName());
+        return "redirect:/event/my";
+    }
+    @PostMapping("/statusUpdate/{id}")
+    public String updateEventPost(
+            @PathVariable("id") Long id,
+            Principal principal,
+            @RequestParam("status") EventStatus eventStatus
+    ){
+        eventService.changeEventStatus(id, eventStatus, principal.getName());
+        return "redirect:/event/my";
+    }
 }
